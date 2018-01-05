c980da7828374097e5f083a2f53b9bbc0acb9799
Momaritti <marietheres.monz@gmail.com>
2018-01-05 11:43:10 +0100

    add components (hear, read, watch)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HearComponent } from './hear/hear.component';
+import { WatchComponent } from './watch/watch.component';
+import { ReadComponent } from './read/read.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HearComponent,
+    WatchComponent,
+    ReadComponent
   ],
   imports: [
     BrowserModule
