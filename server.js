62dbf0c49b70359bea884ad2b6771a23df068f73
elhenro <github@henry-schober.com>
2018-01-12 09:47:15 +0100

    cors and rm jquery with david

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var PouchDB = require('pouchdb');
 var express = require('express');
 var app = express();
+var cors = require('cors');
+
+app.use(cors());
 
 app.use('/db', require('express-pouchdb')(PouchDB));
 
--- src/app/user-system.js
+++ src/app/user-system.js
@@ -1,7 +1,7 @@
-var $ = require('../../node_modules/jquery/dist/jquery.min.js');
-//var PouchDB = require('../../node_modules/pouchdb');
+// var $ = require('../../node_modules/jquery/dist/jquery.min.js');
+// var PouchDB = require('../../node_modules/pouchdb');
 import PouchDB from 'pouchdb';
-PouchDB.plugin(require('pouchdb-authentication'));
+// PouchDB.plugin(require('pouchdb-authentication'));
 var db = new PouchDB('http://localhost:3333/db', {skip_setup: true});
 var local = new PouchDB('local_db');
 local.sync(db, {live: true, retry: true}).on('error', console.log(console));
