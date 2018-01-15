2aae30981990bb089f09fb8a88484a4c7bb53712
elhenro <github@henry-schober.com>
2018-01-15 16:48:49 +0100

    deactivated pouch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, NgZone } from '@angular/core';
-import { PouchDBService } from './pouchdb.service';
+// import { PouchDBService } from './pouchdb.service';
 // import { PouchDb } from 'pouchdb'; // not here
+// import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,46 +10,10 @@ import { PouchDBService } from './pouchdb.service';
 })
 export class AppComponent implements OnInit {
 
-  public people: Array<any>;
-  public form: any;
-
-  public constructor(private database: PouchDBService, private zone: NgZone) {
-    this.people = [];
-    this.form = {
-      'username': '',
-      'firstname': '',
-      'lastname': ''
-    };
-  }
-
   public ngOnInit() {
-    this.database.sync('http://localhost:3333/db');
-    this.database.getChangeListener().subscribe(data => {
-      for (let i = 0; i < data.change.docs.length; i++) {
-        this.zone.run(() => {
-          this.people.push(data.change.docs[i]);
-        });
-      }
-    });
-    this.database.fetch().then(result => {
-      this.people = [];
-      for (let i = 0; i < result.rows.length; i++) {
-        this.people.push(result.rows[i].doc);
-      }
-    }, error => {
-      console.error(error);
-    });
   }
 
   public insert() {
-    if (this.form.username && this.form.firstname && this.form.lastname) {
-      this.database.put(this.form.username, this.form);
-      this.form = {
-        'username': '',
-        'firstname': '',
-        'lastname': ''
-      };
-    }
   }
 
 }
