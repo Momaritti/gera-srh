a9cdd6b59201670be70ec5f812df85194a3254bd
elhenro <github@henry-schober.com>
2018-01-15 13:22:08 +0100

    beginning router setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,42 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 import { PouchDBService } from './pouchdb.service';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HearComponent } from './hear/hear.component';
 import { WatchComponent } from './watch/watch.component';
 import { ReadComponent } from './read/read.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+// router
+const appRoutes: Routes = [
+  { path: 'hear', component: HearComponent },
+  { path: 'watch/:id',      component: WatchComponent },
+  {
+    path: 'read',
+    component: ReadComponent,
+    data: { title: 'Informationen' }
+  }, /*
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }, */
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HearComponent,
     WatchComponent,
-    ReadComponent
+    ReadComponent,
+    PageNotFoundComponent
   ],
-  imports: [
+  imports: [,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     FormsModule,
     HttpModule
