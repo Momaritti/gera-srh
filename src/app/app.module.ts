f09e2e69abc8c0bdea6e03e752bd65ea743cb142
elhenro <github@henry-schober.com>
2018-01-15 17:08:32 +0100

    first working routing setup test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,33 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
-// import { PouchDBService } from './pouchdb.service';
-
 import { HearComponent } from './hear/hear.component';
 import { WatchComponent } from './watch/watch.component';
 import { ReadComponent } from './read/read.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { HomeComponent } from './home/home.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'watch', component: WatchComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HearComponent,
     WatchComponent,
-    ReadComponent
+    ReadComponent,
+    UserListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     // PouchDBService
