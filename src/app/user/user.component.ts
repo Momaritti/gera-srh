0ffaa03e8348722759c87818e569b8c510241940
elhenro <github@henry-schober.com>
2018-02-02 10:01:36 +0100

    userdb json setup

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
@@ -10,14 +11,22 @@ export class UserComponent implements OnInit {
   name: any;
   alter: any;
   sub: any;
-
-  constructor(private route: ActivatedRoute) {
-  }
-
+  results;
+  constructor (private http: HttpClient) { }
+  // constructor(private route: ActivatedRoute) {}
+/*
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.name = params['name'];
       this.alter = params['alter'];
     });
+  }*/
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/userdb').subscribe(
+      data => {
+        this.results = data;
+      }
+    );
   }
 }
