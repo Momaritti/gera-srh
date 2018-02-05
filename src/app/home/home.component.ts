03c7e7a0bbb4b1f906ffa08c30b24e5fc914cd22
elhenro <github@henry-schober.com>
2018-02-05 22:11:53 +0100

    user to be loaded from json server

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
-  user = 'Anna';
-  constructor() { }
-
+  // user = 'Anna';
+  results;
+  public userId;
+  constructor( private route: ActivatedRoute,
+               private http: HttpClient ) {}
   ngOnInit() {
+    let id = 1; // this.route.snapshot.params['id'];
+    this.userId = id;
+    this.http.get('http://localhost:3000/user/' + id ).subscribe(
+      data => {
+        this.results = data;
+      }
+    );
   }
 
 }
